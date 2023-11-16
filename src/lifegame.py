f251785010f605cd20b37bd09ad4db881f14b809
HuachenZH <huachen.zhang.23@eigsi.fr>
2023-11-16 22:29:01 +0100

    add comment and docstring for update()

--- src/lifegame.py
+++ src/lifegame.py
@@ -11,11 +11,10 @@ import pdb
 
 colour_about_to_die = (200, 200, 225)
 colour_alive = (255, 255, 215)
-colour_background = (10, 10, 40)
+colour_background = (10, 10, 10)
 colour_grid = (30, 30, 60)
 
 
-# ------------------------79---------------------------------------------------
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -26,34 +25,44 @@ def get_args():
 
 
 
+# ------------------------79---------------------------------------------------
 def update(surface, cur, sz):
     """Update the canva from current state to next state.
 
             Parameters:
-                    surface (?): seems like a pygame stuff
+                    surface (pygame.surface.Surface): the surface on which
+                    our pygame is displayed.
 
-                    cur (?): current state
+                    cur (np.ndarray): The current state
 
-                    sz (int): cell size
+                    sz (int): Cell size. The number of pixels of a cell's length
 
             Returns:
-                    nxt (?): seems like a pygame stuff. The next state.
+                    next (np.ndarray): The next state.
     """
-    nxt = np.zeros((cur.shape[0], cur.shape[1]))
+    next = np.zeros((cur.shape[0], cur.shape[1]))
 
+    # Iterate through each cell, check one by one if they are going to 
+    # die or live.
     for r, c in np.ndindex(cur.shape):
+        # Count how many live neigbours does the cell have.
         num_alive = np.sum(cur[r-1:r+2, c-1:c+2]) - cur[r, c]
 
+        # Compute the next state:
+        # The cell is living this turn, but is going to die the next turn
+        # because of underpopulation or overpopulation
         if cur[r, c] == 1 and num_alive < 2 or num_alive > 3:
             colour = colour_about_to_die
+        # The live cell will keep living, or the dead cell becomes alive next turn
         elif (cur[r, c] == 1 and 2 <= num_alive <= 3) or (cur[r, c] == 0 and num_alive == 3):
-            nxt[r, c] = 1
+            next[r, c] = 1
             colour = colour_alive
 
+        # Display the current state:
         colour = colour if cur[r, c] == 1 else colour_background
         pygame.draw.rect(surface, colour, (c*sz, r*sz, sz-1, sz-1))
 
-    return nxt
+    return next
 
 
 
@@ -70,7 +79,7 @@ def main(cellsize):
     # initialize pygame    
     pygame.init()
     surface = pygame.display.set_mode((dimx * cellsize, dimy * cellsize))
-    pygame.display.set_caption("____'s Game of Life")
+    pygame.display.set_caption("Conway's Game of Life")
 
 
     # game is on
